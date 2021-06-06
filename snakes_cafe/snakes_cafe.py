39684df85e5f2962750327e8d8b11e4b0d707482
mhendricks96 <mhendricks96@yahoo.com>
2021-06-05 17:56:42 -0700

    completed stretch goals

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -37,30 +37,41 @@ print("***********************************")
 
 # Global Variables
 chosen_items = {}
+menu_options = ("Wings","Cookies","Spring Rolls","Salmon","Steak","Meat Tornado","A Literal Garden","Ice Crea","Cake","Pie","Coffee","Tea","Unicorn Tears",) 
 
 # Functions
 def addChoice(choice):
   
-  print(choice)
-  if choice == "quit":
-    print(f"Goodbye! Here is what you have in your meal {chosen_items}")
+  #print(choice)
+  if choice == "Quit":
+    print("Items added to list:")
+    print('\n'.join("{}: {}".format(k, v) for k, v in chosen_items.items()))
     quit()
-  elif choice in chosen_items:
+  elif choice in chosen_items and menu_options:
     chosen_items[choice] = chosen_items[choice] + 1
     print(f"{chosen_items[choice]} orders of {choice} has been added to your meal")
     askAgain()
-  else:
+  elif choice in menu_options:
     chosen_items[choice] = 1
     print(f"{chosen_items[choice]} order of {choice} has been added to your meal")
     askAgain()
+  else:
+    print("sorry, please choose something on the menu")
 
 def askAgain():
-  userChoice = input("anything else? > ")
-  addChoice(userChoice)   
+  userInput = input("anything else? > ")
+  userChoice = userInput.capitalize()
+  addChoice(userChoice)
+  askAgain()   
 
 #Executable Code
-userChoice = input("> ")
-addChoice(userChoice)
+userInput = input("> ")
+userChoice = userInput.capitalize()
+if userChoice in menu_options:
+  addChoice(userChoice)
+else:
+  print("sorry, please something on the menu")
+  askAgain()
 
 
 
