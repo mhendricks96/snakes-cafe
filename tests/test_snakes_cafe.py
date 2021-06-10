0437f6fcf828dd41de37f3f873ea7c2a47bdeafb
mhendricks96 <mhendricks96@yahoo.com>
2021-06-09 19:08:59 -0700

    trying to work on tests

--- tests/test_snakes_cafe.py
+++ tests/test_snakes_cafe.py
@@ -1,5 +1,11 @@
 from snakes_cafe import __version__
+from snakes_cafe.snakes_cafe import addChoice
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_wrong_item():
+    actual = addCoice("michael")
+    expected = "sorry, please choose something on the menu"
+    assert actual == expected
